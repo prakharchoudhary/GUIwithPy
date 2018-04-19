47e08d398c4a55594c7f8fa951ab6b828d6b13f1
prakharchoudhary <prakhar2397@gmail.com>
2018-04-19 22:29:35 +0530

    add commands for the edit menu options

--- Text Editor/textEditor.py
+++ Text Editor/textEditor.py
@@ -5,6 +5,36 @@ PROGRAM_NAME = "Text Editor"
 
 #### all code goes here ######
 
+
+# Adding Text Widget Built-in Functionality
+
+def cut():
+    content_text.event_generate("<<Cut>>")
+    return "break"
+
+
+def copy():
+    content_text.event_generate("<<Copy>>")
+    return "break"
+
+
+def paste():
+    content_text.event_generate("<<Paste>>")
+    return "break"
+
+
+def undo():
+    content_text.event_generate("<<Undo>>")
+    return "break"
+
+
+def redo(event=None):
+    content_text.event_generate("<<Redo>>")
+    return 'break'
+
+# end of this iteration
+
+
 # Program name
 root.title(PROGRAM_NAME)
 
@@ -21,6 +51,7 @@ redo_icon = PhotoImage(file='./icons/redo.gif')
 # Adding menubar in the widget
 menu_bar = Menu(root)
 
+# adding implementations to 
 file_menu = Menu(menu_bar, tearoff=0)
 file_menu.add_command(label='New', accelerator='Ctrl+N',
                       compound='left', image=new_file_icon, underline=0)
@@ -33,31 +64,63 @@ file_menu.add_separator()
 file_menu.add_command(label='Exit', accelerator='Alt+F4')
 menu_bar.add_cascade(label='File', menu=file_menu)
 
+# end of file menu
 
 edit_menu = Menu(menu_bar, tearoff=0)
 edit_menu.add_command(label='Undo', accelerator='Ctrl+Z',
-                      compound='left', image=undo_icon)
+                      compound='left', image=undo_icon, command=undo)
 edit_menu.add_command(label='Redo', accelerator='Ctrl+Y',
-                      compound='left', image=redo_icon)
+                      compound='left', image=redo_icon, command=redo)
 edit_menu.add_separator()
 edit_menu.add_command(label='Cut', accelerator='Ctrl+X',
-                      compound='left', image=cut_icon)
+                      compound='left', image=cut_icon, command=cut)
 edit_menu.add_command(label='Copy', accelerator='Ctrl+C',
-                      compound='left', image=copy_icon)
+                      compound='left', image=copy_icon, command=copy)
 edit_menu.add_command(label='Paste', accelerator='Ctrl+V',
-                      compound='left', image=paste_icon)
+                      compound='left', image=paste_icon, command=paste)
 edit_menu.add_separator()
 edit_menu.add_command(label='Find', underline=0, accelerator='Ctrl+F')
 edit_menu.add_separator()
 edit_menu.add_command(label='Select All', underline=7, accelerator='Ctrl+A')
 menu_bar.add_cascade(label='Edit', menu=edit_menu)
 
+# end of edit menu
 
 view_menu = Menu(menu_bar, tearoff=0)
-# View menu-items displays some variations
-# so we tackle view menu code to be entered here in a later section
+show_line_number = IntVar()
+show_line_number.set(1)
+view_menu.add_checkbutton(label='Show Line Number', variable=show_line_number)
+show_cursor_info = IntVar()
+show_cursor_info.set(1)
+view_menu.add_checkbutton(label='Show Cursor Location at Bottom', variable=show_cursor_info)
+highlight_line = IntVar()
+view_menu.add_checkbutton(label='Highlight Current Line',
+	onvalue=1, offvalue=0, variable=highlight_line)
+themes_menu = Menu(menu_bar, tearoff=0)
+view_menu.add_cascade(label='Themes', menu=themes_menu)
+
+"""
+color scheme is defined with dictionary elements like -
+        theme_name : foreground_color.background_color
+"""
+color_schemes = {
+    'Default': '#000000.#FFFFFF',
+    'Greygarious': '#83406A.#D1D4D1',
+    'Aquamarine': '#5B8340.#D1E7E0',
+    'Bold Beige': '#4B4620.#FFF0E1',
+    'Cobalt Blue': '#ffffBB.#3333aa',
+    'Olive Green': '#D1E7E0.#5B8340',
+    'Night Mode': '#FFFFFF.#000000',
+}
+
+
+theme_choice = StringVar()
+theme_choice.set('Default')
+for k in sorted(color_schemes):
+    themes_menu.add_radiobutton(label=k, variable=theme_choice)
 menu_bar.add_cascade(label='View', menu=view_menu)
 
+# End of view menu
 
 about_menu = Menu(menu_bar, tearoff=0)
 about_menu.add_command(label='About')
@@ -81,6 +144,10 @@ scroll_bar = Scrollbar(content_text)
 content_text.configure(yscrollcommand=scroll_bar.set)
 scroll_bar.config(command=content_text.yview)
 scroll_bar.pack(side='right', fill='y')
+
+# handling redo quirk
+content_text.bind('<Control-y>', redo)
+content_text.bind('<Control-Y>', redo)
 
 ##############################
 root.mainloop()
